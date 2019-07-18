a5f5aadcab75f0b8c65d596f3a92e5cd7c2ff644
Liuchao <34504020@qq.com>
2019-07-18 16:32:45 +0800

    123

--- src/Wechat.php
+++ src/Wechat.php
@@ -1,16 +1,40 @@
 <?php
-
 namespace Hitomi;
 
 class Wechat{
 
+    static protected $instance = null;
 
-    public function __construct(){
+    // 主要配置参数
+    protected $appId = '';
+    protected $appSecret = '';
+    protected $mchId = '';
+    protected $key = '';
 
+    // 构造方法
+    protected function __construct($options = []){
+        foreach ($options as $name => $item) {
+            if (property_exists($this, $name)) {
+                $this->$name = $item;
+            }
+        }
+    }
+
+
+    // 单例模式 实例化
+    public static function instance($options = []){
+        if (is_null(self::$instance)) {
+            self::$instance = new static($options);
+        }
+        return self::$instance;
     }
 
 
     public function foo(){
-        echo 'foo demo2 demo3 decd1 222 3333333';
+        echo 'test : ' . $this->appId;
+    }
+
+    public function getAccessToken(){
+        echo 'accessToken';
     }
 }
